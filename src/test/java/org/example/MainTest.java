f754b2430213f0056eef4f1d71b07d1461b7f76a
FrancisCoDeGrano <franciscodgrano@gmail.com>
2024-10-14 20:31:03 -0400

    R-TEST-001 - RESP-1

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    public void setUp() {
+        game = new Game();  // This will initialize the game
+    }
+
+    @Test
+    public void RESP_1_test_1_setUpAdventureAndEventDecks() {
+        game.setUpDecks();  // Method to be implemented
+
+        // Check the number of cards in each deck and distribution of card types
+        assertNotNull(game.getAdventureDeck());
+        assertEquals(100, game.getAdventureDeck().size());
+
+        assertNotNull(game.getEventDeck());
+        assertEquals(17, game.getEventDeck().size());
+    }
+}
